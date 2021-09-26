46721412df3509b0b4835ebb8fac1b61cb999a07
ichxorya <benghia1st@gmail.com>
2021-09-26 17:40:03 +0700

    i'm a chad programmer now

--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -1,28 +1,55 @@
 import java.util.Scanner;
 
 public class DictionaryManagement {
+    /**
+     * Variables.
+     */
     static Dictionary book;
+    Scanner sc = new Scanner(System.in);
+
+    /**
+     * Methods.
+     */
+
+    /* Return a non-empty String */
+    String nonEmptyString() {
+        String aWord = sc.nextLine();
+        return  aWord;
+    }
 
     void insertFromCommandline() {
-        Scanner sc = new Scanner(System.in);
+        /* Variables */
         int numberOfWord = -1;
         int dictIndex = 0;
+        String wordEn = "";
+        String wordVie = "";
+        String autoFlush = "";     // Important variable meow~~
 
+        /* Input */
         while (numberOfWord <= 0) {
             System.out.println("How many words are there?");
             System.out.print("(Input a positive number): ");
-            numberOfWord = sc.nextInt();
+            numberOfWord = sc.nextInt();    // After using sc.nextInt(), we have to flush (?) the input stream
         }
+        autoFlush = sc.nextLine();          // Flushed!
 
         book.dict = new Word[numberOfWord];
         while (numberOfWord > 0) {
             System.out.println("Input the English word and its Vietnamese translation: ");
-            String wordEn = sc.nextLine();
-            String wordVie = sc.nextLine();
+            wordEn = sc.nextLine();
+            wordVie = sc.nextLine();
 
-            System.out.println(wordEn + " va " + wordVie);
+            /* Non-empty check */
+            while (wordEn.equals("")) {
+                System.out.println("<!> Re-input the English word <!>");
+                wordEn = sc.nextLine();
+            }
+            while (wordVie.equals("")) {
+                System.out.println("<!> Re-input the Vietnamese translation <!>");
+                wordVie = sc.nextLine();
+            }
 
-
+            /* Added to the dictionary */
             book.dict[dictIndex] = new Word(wordEn, wordVie);
 
             ++dictIndex;
