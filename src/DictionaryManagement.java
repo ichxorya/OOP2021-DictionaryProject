a29183c035c974a82545ddab1bf449843d61d9bc
ichxorya <benghia1st@gmail.com>
2021-09-28 00:51:09 +0700

    0h50 ngày 28 tháng 9. Mắt tôi đang mờ dần đi, và tạm fix cái bug thiếu phần tử khi khởi tạo mảng bằng việc để file txt có thêm một dòng "đệm" trên cùng và khởi tạo mảng có N - 1 phần tử (N là số dòng)

--- src/DictionaryManagement.java
+++ src/DictionaryManagement.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.nio.*;
 import java.util.Scanner;
 
 public class DictionaryManagement {
@@ -8,31 +7,28 @@ public class DictionaryManagement {
      * More features (not bugs) will be implemented in the near future.
      */
 
-
-    /** Variables. */
-    Scanner sc = new Scanner(System.in);    // Using scanner to receive user inputs.
-
     /** Methods. */
     /* Insert From Command Line */
     void insertFromCommandline() {
         /* Variables */
-        int numberOfWord = -1;
+        Scanner sc = new Scanner(System.in);    // Using scanner to receive user inputs.
+        int numberOfWords = -1;
         int dictIndex = 0;
         String wordEn;
         String wordVie;
 
         /* Input */
-        while (numberOfWord <= 0) {
+        while (numberOfWords <= 0) {
             System.out.println("How many words are there?");
             System.out.print("(Input a positive number): ");
-            numberOfWord = sc.nextInt();    // After using sc.nextInt(), we have to flush (?) the input stream
+            numberOfWords = sc.nextInt();    // After using sc.nextInt(), we have to flush (?) the input stream
         }
         // Flushed!
         // Important variable meow~~
         String autoFlush = sc.nextLine();
 
-        Dictionary.dictionary = new Word[numberOfWord];
-        while (numberOfWord > 0) {
+        Dictionary.dictionary = new Word[numberOfWords];
+        while (numberOfWords > 0) {
             System.out.println("Input the English word and its Vietnamese translation: ");
             wordEn = sc.nextLine();
             wordVie = sc.nextLine();
@@ -51,23 +47,45 @@ public class DictionaryManagement {
             Dictionary.dictionary[dictIndex] = new Word(wordEn, wordVie);
 
             ++dictIndex;    // Increase the index of the Word array
-            --numberOfWord; // Reduce the number of Word in the queue by 1
+            --numberOfWords; // Reduce the number of Word in the queue by 1
         }
     }
 
     /* Insert From File */
     void insertFromFile() {
+        Scanner sc;    // Using scanner to receive data from external file.
+        Scanner parser;    // Using scanner to parse strings from external file.
+        int numberOfWords;
+        int dictIndex = 0;
+        String wordEn;
+        String wordVie;
+        String wordLine;
+
         try {
             // Open file
             File dictionaryFile = new File("src/dictionaries.txt");
-            FileInputStream dictionaryFileStream = new FileInputStream(dictionaryFile);
-            BufferedInputStream reader = new BufferedInputStream(dictionaryFileStream);
 
-            // Number of Lines in file = Number of elements in Dictionary (array)
-            int dictionarySize = numberOfLines(dictionaryFile);
-            System.out.println(dictionarySize);
+            // Number of Lines in file = Number of Words
+            numberOfWords = numberOfLines(dictionaryFile);
+            Dictionary.dictionary = new Word[numberOfWords - 1];
 
-            // Close file
+            // Read file
+            sc = new Scanner(dictionaryFile);
+
+            // Imply that the input file is P E R F E C T
+            while (sc.hasNextLine() && dictIndex < numberOfWords) {
+                wordLine = sc.nextLine();
+                parser = new Scanner(wordLine);
+                parser.useDelimiter("\t");
+
+                wordEn = sc.next();
+                wordVie = sc.next();
+                Dictionary.dictionary[dictIndex] = new Word(wordEn, wordVie);
+//                System.out.println(wordEn + " " + wordVie);
+                ++dictIndex;    // Increase the index of the Word array
+            }
+
+            // Close file (automated???)
         } catch (IOException e) {
             System.out.println("<!> Make sure you have dictionaries.txt in the src folder <!>");
 //            e.printStackTrace();  for debugging
