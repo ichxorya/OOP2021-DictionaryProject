c3bcfd8926ab178f49a8302501ad12e8beffd002
coder-newbie404 <74915494+coder-newbie404@users.noreply.github.com>
2021-10-20 21:14:35 +0700

    quick update v2

--- src/main/java/com/example/tree_dictionary_app/Application.java
+++ src/main/java/com/example/tree_dictionary_app/Application.java
@@ -1,21 +1,20 @@
 package com.example.tree_dictionary_app;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("test-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("test-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Dictionary!");
         stage.setScene(scene);
         stage.show();
-        
+
     }
 
     public static void main(String[] args) {
--- src/main/java/com/example/tree_dictionary_app/UIController.java
+++ src/main/java/com/example/tree_dictionary_app/UIController.java
@@ -6,7 +6,7 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class HelloController {
+public class UIController {
 
     DictionaryManagementUI dictionary = new DictionaryManagementUI();
 
