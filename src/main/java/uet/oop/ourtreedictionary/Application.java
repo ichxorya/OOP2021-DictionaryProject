1864978038e4d5f83c10bfc07a6fe43293fab1a1
xor <benghia1st@gmail.com>
2021-10-24 22:15:07 +0700

    tèo teo tèo teo téo

--- src/main/java/uet/oop/ourtreedictionary/Application.java
+++ src/main/java/uet/oop/ourtreedictionary/Application.java
@@ -5,7 +5,9 @@ import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
+import java.awt.*;
 import java.io.IOException;
+import java.net.URI;
 import java.util.Objects;
 
 public class Application extends javafx.application.Application {
@@ -13,11 +15,34 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("dictGUI.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
+
+        // Prevent fullscreen
+        stage.maximizedProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                stage.setMaximized(false);
+                ThEbEsTfEaTuRe();
+            }
+        });
+
         stage.setTitle("Un-Dictionary!");
         stage.setScene(scene);
         stage.getIcons().add(appIcon());
 
         stage.show();
+    }
+
+    private void ThEbEsTfEaTuRe() {
+        try {
+            Desktop desktop = java.awt.Desktop.getDesktop();
+            URI oURL = new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ%22");
+            try {
+                desktop.browse(oURL);
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private Image appIcon() {
---
+++ src/main/java/uet/oop/ourtreedictionary/Bube.java
@@ -0,0 +1,11 @@
+package uet.oop.ourtreedictionary;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URI;
+
+public class Bube {
+    public static void main(String[] args) {
+
+    }
+}
